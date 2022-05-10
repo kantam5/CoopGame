579790c3c1e36fa51bbbd9643cefda2681d7e862
Kantam5 <wlsry45246@gmail.com>
2022-05-10 12:28:23 +0900

    Add Custom Event

--- Source/CoopGame/Public/Components/SHealthComponent.h
+++ Source/CoopGame/Public/Components/SHealthComponent.h
@@ -6,6 +6,8 @@
 #include "Components/ActorComponent.h"
 #include "SHealthComponent.generated.h"
 
+// OnHealthChanged Event가 blueprint에 생긴다
+DECLARE_DYNAMIC_MULTICAST_DELEGATE_SixParams(FOnHealthChangedSignature, USHealthComponent*, HealthComp, float, Health, float, HealthDeltaconst, const class UDamageType*, DamageType, class AController*, InstigatedBy, AActor*, DamageCauser);
 
 UCLASS( ClassGroup=(COOP), meta=(BlueprintSpawnableComponent) )
 class COOPGAME_API USHealthComponent : public UActorComponent
@@ -28,4 +30,8 @@ protected:
 		
 	UFUNCTION()
 	void HandleTakeAnyDamage(AActor* DamagedActor, float Damage, const class UDamageType* DamageType, class AController* InstigatedBy, AActor* DamageCauser);
+
+public:
+	UPROPERTY(BlueprintAssignable, Category = "Events")
+	FOnHealthChangedSignature OnHealthChanged;
 };
