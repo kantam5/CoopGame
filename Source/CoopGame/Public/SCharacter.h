bc92cfd869b9e14190a748093132bdf9802aff33
Kantam5 <wlsry45246@gmail.com>
2022-05-10 23:36:34 +0900

    Create 'Gears of War'-style Health Indicator Material

--- Source/CoopGame/Public/SCharacter.h
+++ Source/CoopGame/Public/SCharacter.h
@@ -42,6 +42,7 @@ protected:
 	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Components")
 	USpringArmComponent* SpringArmComp;
 
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Components")
 	USHealthComponent* HealthComp;
 
 	bool bWantsToZoom;
